d0ffbb06ce3012ceee7d2c532efb23af8c4d8e15
Suman Sourav Singh <sumansouravsingh999@gmail.com>
2025-06-03 14:11:50 -0500

    removed key strokes

--- src/wake_mate.py
+++ src/wake_mate.py
@@ -11,14 +11,15 @@ from pathlib import Path
 # -------------------- Configuration --------------------
 
 ICON_DIR = Path("icons")
-MOVE_INTERVAL = 60  # seconds between actions
+MOVE_INTERVAL = 59  # seconds between actions
 MOVE_DURATION = 1  # duration of mouse movement in seconds
 POS_X, POS_Y = 5, 5
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class WakeMateApp:
-    """Class """
+    """Class that handles GUI controls and periodic mouse movement to prevent system sleep."""
+
     def __init__(self, root):
         """Initialize the WakeMateApp.
             Args:
@@ -61,33 +62,48 @@ class WakeMateApp:
         play_button = tk.Button(self.root, image=self.play_img, command=self.start)
         pause_button = tk.Button(self.root, image=self.pause_img, command=self.pause)
         stop_button = tk.Button(self.root, image=self.stop_img, command=self.stop)
-
         play_button.grid(row=0, column=0, padx=20, pady=10)
         pause_button.grid(row=0, column=1, padx=20, pady=10)
         stop_button.grid(row=0, column=2, padx=20, pady=10)
 
-    def _worker(self):
+    def __worker(self):
         """
-            Background thread function that moves the mouse and types periodically.
+            Background thread function that moves the mouse periodically.
             Continues until the `self.is_running` flag is set to False.
         """
+
         try:
+            if sys.platform == "win32":
+                import ctypes
+                ES_CONTINUOUS = 0x80000000
+                ES_SYSTEM_REQUIRED = 0x00000001
+                ES_DISPLAY_REQUIRED = 0x00000002
+                ctypes.windll.kernel32.SetThreadExecutionState(
+                    ES_CONTINUOUS | ES_SYSTEM_REQUIRED | ES_DISPLAY_REQUIRED)
             while self.is_running:
                 self.current_x = self.current_x + 1 if self.current_x + 1 < self.screen_width else POS_X
                 self.current_y = self.current_y + 1 if self.current_y + 1 < self.screen_height else POS_Y
                 pyautogui.moveTo(self.current_x, self.current_y, duration=MOVE_DURATION)
-                logging.info("Mouse moved and text typed.")
+                time.sleep(1)
+                pyautogui.moveTo(self.screen_width//2, self.screen_height//2, duration=MOVE_DURATION)
+                logging.info("Mouse moved.")
                 time.sleep(MOVE_INTERVAL)
         except Exception as e:
             logging.exception("Error in worker thread: %s", e)
+
+        finally:
+            if sys.platform == "win32":
+                logging.info("Net")
+                #ctypes.windll.kernel32.SetThreadExecutionState(0x80000000)
+            logging.info("Worker thread exited")
 
     def start(self):
         """Starts the background automation by launching the worker thread."""
         if not self.is_running:
             self.is_running = True
-            self.worker_thread = threading.Thread(target=self._worker, daemon=True)
+            logging.info(f"Platform is: {sys.platform}")
+            self.worker_thread = threading.Thread(target=self.__worker, daemon=True)
             self.worker_thread.start()
-            messagebox.showinfo("Action", "Started")
             logging.info("Automation started.")
 
     def pause(self):
@@ -102,6 +118,8 @@ class WakeMateApp:
         if self.is_running:
             self.is_running = False
             logging.info("Automation stopped.")
+            if self.worker_thread and self.worker_thread.is_alive():
+                self.worker_thread.join(timeout=1)
         self.current_x, self.current_y = POS_X, POS_Y
         messagebox.showinfo("Action", "Stopped and Reset")
         logging.info("Position reset.")
